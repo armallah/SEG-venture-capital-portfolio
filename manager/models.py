78174898cccb52aaec983451faa0d7fbc0cef76e
Thatchathon Leelawat <Iamninnin@gmail.com>
2023-02-13 17:37:41 +0000

    Adding in new models

---
+++ manager/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from models import relations
+# Create your models here.
+
+class Entity(models.Model):
+    name = models.CharField(max_length=50)
+
+    ## The following is made only to appease the type checker (No special stuff here)
+    invested_company: models.QuerySet["Company"]
+    founding_company: models.QuerySet["Company"]
+    def getName(self):
+        return self.name
+
+class Company(models.Model):
+    name = models.CharField(max_length=50)
+    number = models.CharField(max_length=50, blank=False, null=False)
+    country_code = models.CharField(max_length=15)
+    investors = models.ManyToManyField(
+            Entity,
+            through='Investing',
+            through_fields=('company','investor'),
+            related_name='invested_company'
+        )
+    founders = models.ManyToManyField(
+            Entity,
+            related_name='founding_company'
+            )
+    wayra_right: models.Manager["Right"]
+
+
+
+class Investing(models.Model):
+    investor = models.ForeignKey(Entity, on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+    amount = models.DecimalField(max_digits = 20, decimal_places=3)
+
+class Right(models.Model):
+    name = models.CharField(max_length=50)
+    holding_right = models.ManyToManyField(Company, related_name="wayra_right")
+    
