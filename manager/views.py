322b68ffeedf7574f788109ddb92fbd79cc45f0f
Abdul Rahman <armallah1978@gmail.com>
2023-02-23 17:11:39 +0000

    make login page setup

--- manager/views.py
+++ manager/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def home(request):
+    return HttpResponse("Hello, home page here please.")
+    
+def log_in(request):
+    return render(request, 'log_in.html', {'form': form})
+ 
--- portfolio_management/urls.py
+++ portfolio_management/urls.py
@@ -16,5 +16,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from manager import views
 
-urlpatterns = [path("admin/", admin.site.urls)]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('', views.home , name="home"),
+    path("log_in" , views.log_in , name="log in")
+]
+
+
