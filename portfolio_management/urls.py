0e5351d50f2185a58de5998f8b02705f2858e7ad
= <sparsh.patel@kcl.ac.uk>
2023-02-25 21:30:45 +0000

    Add Base templates

--- portfolio_management/urls.py
+++ portfolio_management/urls.py
@@ -16,5 +16,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from manager import views
 
-urlpatterns = [path("admin/", admin.site.urls)]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('base/', views.base, name = 'base'),
+]
