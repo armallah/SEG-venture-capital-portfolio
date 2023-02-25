adaef2d3efcd85cfe5ec910d692708b6c12a4310
Abdul Rahman <armallah1978@gmail.com>
2023-02-25 00:24:46 +0000

    add to login page

--- manager/forms.py
+++ manager/forms.py
@@ -1,4 +1,8 @@
 from django import forms
+from .models import *
+from django.core.validators import RegexValidator
+
 
 class LoginForm(forms.Form):
-    pass
+    username = forms.CharField(label='Username')
+    password = forms.CharField(label='Password', widget=forms.PasswordInput())
--- manager/views.py
+++ manager/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.http import HttpResponse
+from .forms import LoginForm
+from django.contrib.auth import authenticate , login
+from django.contrib import messages
 
 # Create your views here.
 
@@ -8,5 +11,18 @@ def home(request):
     return HttpResponse("Hello, home page here please.")
     
 def login(request):
-    return render(request, 'login.html')
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.add_message(request, messages.ERROR, "The credentials provided were invalid!")
+            
+    form = LoginForm()
+    return render(request, 'login.html' , {'form':form})
  
