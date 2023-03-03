ef61cef957cbc2d5bad76675d87dc8f447951c1b
= <sparsh.patel@kcl.ac.uk>
2023-03-03 21:31:28 +0000

    Update views.py

--- manager/views.py
+++ manager/views.py
@@ -3,20 +3,24 @@ from django.shortcuts import render
 # Create your views here.
 def dashboard(request):
     context = {
+
     }
     return render(request, 'dashboard.html', context)
 
 def portfolio(request):
     context = {
+
     }
     return render(request, 'portfolio.html', context)
 
 def entities(request):
     context = {
+
     }
     return render(request, 'entities.html', context)
 
 def founders(request):
     context = {
+    
     }
     return render(request, 'founders.html', context)
