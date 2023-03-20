77d11d204c910ae0f35949797b58866c9f90928a
= <sparsh.patel@kcl.ac.uk>
2023-03-20 13:11:22 +0000

    Update forms.py

--- manager/forms.py
+++ manager/forms.py
@@ -3,9 +3,9 @@ from .models import *
 from django.core.validators import RegexValidator
 from django.forms import ModelForm
 
-        
+
 class LoginForm(forms.Form):
-    
+
     username_attrs = {
         'id':'username',
         'name': 'username',
@@ -13,7 +13,7 @@ class LoginForm(forms.Form):
         'id':'username',
         'placeholder':"Username"
     }
-    
+
     password_attrs = {
         'id':'password',
         'name':'password',
@@ -21,12 +21,51 @@ class LoginForm(forms.Form):
         'id':'password',
         'placeholder':"************"
     }
-        
+
     username = forms.CharField(label='', widget=forms.TextInput(attrs=username_attrs))
     password = forms.CharField(label='', widget=forms.PasswordInput(attrs=password_attrs))
-        
+
 class DocumentForm(ModelForm):
     class Meta:
         model = Document
         fields = ['upload']
 
+class AddNewUser(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
+        widgets = {
+            # 'password': forms.PasswordInput(),
+        }
+        help_texts = { 'first_name': None, 'last_name': None, 'email': None}
+    # username = forms.CharField(label="Email")
+    password = forms.CharField(
+        label = 'Password',
+        widget = forms.PasswordInput(),
+        validators = [ # validates that the input matches the requirements
+            RegexValidator(
+                regex = r'^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).*$',
+                message = 'Password must contain an uppercase character, a lowercase character and a number'
+            ),
+        ]
+    )
+    password_confirmation = forms.CharField(label = 'Password Confirmation', widget = forms.PasswordInput())
+
+    def clean(self): # checks if the password matches the confirm password
+        super().clean()
+        password = self.cleaned_data.get('password')
+        password_confirmatiuon = self.cleaned_data.get('password_confirmation')
+        if password != password_confirmatiuon:
+            self.add_error('password_confirmation', 'Confirmation password does not match the password.') # creates an erro
+
+    def save(self): # creates a new user
+        super().save(commit=False)
+        user = User.objects.create_user(
+            self.cleaned_data.get('email'),
+            first_name = self.cleaned_data.get('first_name'),
+            last_name = self.cleaned_data.get('last_name'),
+            email = self.cleaned_data.get('email'),
+            password = self.cleaned_data.get('password'),
+            user_type = 1
+        )
+        return
