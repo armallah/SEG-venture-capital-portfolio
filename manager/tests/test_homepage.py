543b7f0f6ebee2b7ba32668bbf5a5cd601220642
Umair402 <umair.qureshi@hotmail.co.uk>
2023-03-21 15:43:08 +0000

    test homepage renders

--- manager/tests/test_homepage.py
+++ manager/tests/test_homepage.py
@@ -1,5 +1,12 @@
 from django.test import TestCase, Client
+from django.urls import reverse
 
 class homePageTest(TestCase):
     def setUp(self):
         self.client = Client()
+
+    def test_homepage_renders(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
