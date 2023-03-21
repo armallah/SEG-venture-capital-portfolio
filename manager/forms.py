de30fb11250ee8681479df2c3258e55c66fdea1e
Pratt-Gyrotron <87197648+Pratt-Gyrotron@users.noreply.github.com>
2023-03-21 13:14:07 +0000

    Update forms.py

--- manager/forms.py
+++ manager/forms.py
@@ -41,3 +41,43 @@ class CompanyForm(ModelForm):
         }
         """
 
+class AddNewUser(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
+        widgets = {
+            # 'password': forms.PasswordInput(),
+        }
+        help_texts = { 'first_name': None, 'last_name': None, 'email': None}
+    # username = forms.CharField(label="Email")
+    password = forms.CharField(
+        label = 'Password',
+        widget = forms.PasswordInput(),
+        validators = [ # validates that the input matches the requirements
+            RegexValidator(
+                regex = r'^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).*$',
+                message = 'Password must contain an uppercase character, a lowercase character and a number'
+            ),
+        ]
+    )
+    password_confirmation = forms.CharField(label = 'Password Confirmation', widget = forms.PasswordInput())
+
+    def clean(self): # checks if the password matches the confirm password
+        super().clean()
+        password = self.cleaned_data.get('password')
+        password_confirmatiuon = self.cleaned_data.get('password_confirmation')
+        if password != password_confirmatiuon:
+            self.add_error('password_confirmation', 'Confirmation password does not match the password.') # creates an erro
+
+    def save(self): # creates a new user
+        super().save(commit=False)
+        user = User.objects.create_user(
+            self.cleaned_data.get('email'),
+            first_name = self.cleaned_data.get('first_name'),
+            last_name = self.cleaned_data.get('last_name'),
+            email = self.cleaned_data.get('email'),
+            password = self.cleaned_data.get('password'),
+            user_type = 1
+        )
+        return
+
