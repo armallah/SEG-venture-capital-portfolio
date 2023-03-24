84f314002f83e22e0dfc821afa27ca13ddb2f8d8
Thatchathon Leelawat <Iamninnin@gmail.com>
2023-03-24 00:56:27 +0000

    add automatic admin

--- manager/apps.py
+++ manager/apps.py
@@ -1,6 +1,23 @@
 from django.apps import AppConfig
 
 
+
 class ManagerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'manager'
+    name = 'manager'    
+
+    def ready(self):
+
+        from manager.models import User
+        try:
+            User.objects.create_superuser(
+                    username="admin@wayra.co"
+                    ,password="password"
+                    ,first_name = "Wayra"
+                    ,last_name = "Admin"
+                    ,user_type = 2
+                    )
+        except:
+            print("hi")
+
+        pass # startup code here
